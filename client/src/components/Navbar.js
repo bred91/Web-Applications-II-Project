c39aeca405ef47000ba5932887be8e88aa39d77a
unknown <simransingh403769@gmail.com>
2023-04-13 20:35:10 +0200

    fix: nav link in navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,25 +2,36 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Navbar, Nav } from "react-bootstrap";
 
-
 function TSNavbar() {
-    return (
-        <Navbar bg="light">
-          <Container>
-            <Navbar.Brand href="/">Ticketing Platform</Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link href="/searchProfile">Search Profile</Nav.Link>
-                <Nav.Link href="/searchProduct">Search Product</Nav.Link>
-                <Nav.Link href="/createProduct">Create Product</Nav.Link>
-            </Nav>
-            <Navbar.Collapse className="justify-content-end">
-                <Link to="/signup">
-                    <button className="btn btn-primary"> Signup </button>
-                </Link>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      );
+  return (
+    <Navbar bg="light">
+      <Container>
+        <Navbar.Brand href="/">Ticketing Platform</Navbar.Brand>
+        <Nav className="mx-auto">
+          <Nav.Item>
+            <Nav.Link as={Link} to="/searchProfile">
+              Search Profile
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/searchProduct">
+              Search Product
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/createProduct">
+              Create Product
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Navbar.Collapse className="justify-content-end">
+          <Link to="/signup">
+            <button className="btn btn-primary"> Signup </button>
+          </Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default TSNavbar;
