61ea95f495169c6c30e07eab6753cfc1e005f722
bred91 <paneraf02@gmail.com>
2023-04-21 16:23:18 +0200

    feat: added ticketing module

---
+++ ticketing/src/test/kotlin/it/polito/ticketing/TicketingApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.ticketing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TicketingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
