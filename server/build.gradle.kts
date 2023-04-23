eda8039e5723291b1629570e49fdb340fbe9ce43
bred91 <paneraf02@gmail.com>
2023-04-23 12:29:04 +0200

    feat: added "noarg" anf "allopen" plugins

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
 	kotlin("plugin.jpa") version "1.7.22"
+	id("org.jetbrains.kotlin.plugin.noarg") version "1.8.20"
+	id("org.jetbrains.kotlin.plugin.allopen") version "1.8.20"
 }
 
 group = "it.polito"
