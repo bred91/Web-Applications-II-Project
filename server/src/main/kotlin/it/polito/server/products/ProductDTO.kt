f8feb1dbe5b96b95a4f54a7bdab6397c1727c997
bred91 <paneraf02@gmail.com>
2023-04-26 16:16:18 +0200

    refactor: deleting Ticket module

--- server/src/main/kotlin/it/polito/server/products/ProductDTO.kt
+++ server/src/main/kotlin/it/polito/server/products/ProductDTO.kt
@@ -7,13 +7,13 @@ data class ProductDTO(
 
     @field:NotBlank(message="Not a valid ean")
     @field:Size(max = 23, message = "EAN too long")
-    var ean: String,
+    val ean: String,
     @field:NotBlank(message="Not a valid name")
     @field:Size(max = 255, message = "Product name too long")
-    var name: String,
+    val name: String,
     @field:NotBlank(message="Not a valid brand")
     @field:Size(max = 255, message = "Brand name too long")
-    var brand: String
+    val brand: String
 )
 
 fun ProductDTO.toEntity(): Product{
