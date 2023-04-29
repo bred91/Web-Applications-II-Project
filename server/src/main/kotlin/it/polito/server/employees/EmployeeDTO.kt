6d8999575b71226514486a37170e9ac1588efd82
DIMITRI MASETTA <S306130@studenti.polito.it>
2023-04-29 15:24:37 +0200

    Add, EmployeeDTO

---
+++ server/src/main/kotlin/it/polito/server/employees/EmployeeDTO.kt
@@ -0,0 +1,35 @@
+package it.polito.server.employees
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class EmployeeDTO(
+    val id: Long?,
+    @field:NotBlank(message="Not a valid name")
+    @field:Size(max = 255, message = "Name too long")
+    val name: String,
+    @field:NotBlank(message="Not a valid surname")
+    @field:Size(max = 255, message = "Surname too long")
+    val surname: String,
+    @field:NotBlank(message="Not a valid email")
+    @field:Size(max = 255, message = "Email too long")
+    val email: String,
+
+    val role: RoleDTO?
+){
+    constructor(name: String, surname: String, email: String, role: RoleDTO?)
+        :this(null, name, surname, email, role)
+}
+
+fun EmployeeDTO.toEntity(): Employee{
+    val employee = Employee()
+    employee.id = id
+    employee.name = name
+    employee.surname = surname
+    employee.email = email
+    employee.role = role.toEntity()
+    return employee
+}
+
+fun Employee.toDTO() = EmployeeDTO(id, name, surname, email, role)
+
