30f31c293224a0626f8ca8b0f3c0e4eb6bf7564d
DIMITRI MASETTA <S306130@studenti.polito.it>
2023-06-12 16:25:03 +0200

    Update: Client, small fix

--- client/src/components/CreateTicket.js
+++ client/src/components/CreateTicket.js
@@ -133,7 +133,7 @@ function CreateTicketComponent(props){
                         </Form.Group>
                         <Form.Group controlId="note">
                             <Form.Label>Additional note</Form.Label>
-                            <Form.Control type="text" value={note} onChange={(event) => setNote(event.target.value)}/>
+                            <Form.Control as="textarea" value={note} onChange={(event) => setNote(event.target.value)}/>
                         </Form.Group>
                         <Button variant="success" className="w-100" type="submit">Submit ticket</Button>
                     </Form>
